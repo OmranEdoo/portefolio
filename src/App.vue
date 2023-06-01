3a5afcac984224911b12d6475d48f57ebacb62c0
zumbalove974 <omranedoo@gmail.com>
2023-06-01 19:40:38 +0200

    spotlight

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <v-app id="appContainer">
+    <div class="spotlight"></div>
     <div class="rowContainer">
       <div class="columnContainer">
         <div id="presContainer">
-          <h1>Omran Edoo</h1>
+          <h1 id="blaze">Omran Edoo</h1>
           <TypeWriter :array="descriptions" />
         </div>
         <div class="rowContainer">
           <div id="projectsContainer">
             <ProjectCarousel />
           </div>
-          <p>{{ projectText }}</p>
+          <div class="columnContainer overflow-y-auto v-navigation-drawer__content mr-8 mt-8" style="height: 50vh;">
+            <h3 class="pb-4">{{ title }}</h3>
+            <p v-for="text in projectText" :key="text" class="pb-4" style="text-align: justify">
+              {{ text }}
+            </p>
+          </div>
         </div>
       </div>
       <div id="cursusContainer">
@@ -38,9 +44,25 @@ export default {
   data() {
     return {
       descriptions: ["Future ingénieur...", "Passionné de programmation...", "Joueur d'échecs (très) amateur..."],
-      projectText: "Ce projet etieshbsdjhbvk ndvhbjsd kj dvhkdjhb  bj jbj bjbjhvj"
+      title: "TraiLove76200",
+      projectText: ["Le projet TrailLove76200 vise à visualiser les trajectoires des participants à la course Oxfam Trailwalker 2021 et à fournir une analyse spatio- temporelle de ces trajectoires. Le contexte du projet est l’exploration des données collectées pendant la course pour identifier les difficultés rencontrées par les participants et fournir des analyses de performance détaillées.Le principal défi est de trouver un moyen efficace de visualiser ces données et d’offrir des outils d’analyse aux organisateurs de la course et aux chercheurs intéressés par les données collectées. Pour relever ce défi, le projet propose le développement d’une application web performante capable de représenter l’évolution de l’allure des participants en fonction de différents paramètres.", "L’application permet une visualisation 2D et 2D + 1 claires et précises des données, ainsi qu’une analyse spatio - temporelle des trajectoires.Elle permet une prise en main rapide par les utilisateurs grâce aux différents aides fournis."]
     }
   },
+  mounted() {
+    window.addEventListener("DOMContentLoaded", () => {
+      const spotlight = document.querySelector('.spotlight');
+      //let spotlightSize = 'transparent 50px, rgba(0, 0, 0, 0.5) 200px)';
+
+      //spotlight.style.backgroundImage = `radial-gradient(circle at ${window.innerWidth / 2}px ${window.innerHeight / 2}px, ${spotlightSize}`;
+      window.addEventListener('mousemove', e => updateSpotlight(e));
+
+      function updateSpotlight(e) {
+        spotlight.style.left = `${e.pageX / window.innerWidth * 100 - 25}%`
+        spotlight.style.top = `${e.pageY / window.innerHeight * 100 - 50}%`
+        //spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+      }
+    });
+  }
 }
 </script>
 
@@ -96,5 +118,39 @@ body,
 .columnContainer {
   display: flex;
   flex-direction: column;
+}
+
+@font-face {
+  font-family: "title";
+  src: url("./fonts/MontserratBlack.ttf");
+}
+
+#blaze {
+  text-transform: uppercase;
+  font-family: title;
+  font-size: 50px;
+}
+
+.v-navigation-drawer__content::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px #5d5d5d;
+  background-color: #5d5d5d;
+}
+
+.v-navigation-drawer__content::-webkit-scrollbar {
+  width: 0px;
+}
+
+.v-navigation-drawer__content::-webkit-scrollbar-thumb {
+  -webkit-box-shadow: inset 0 0 6px #424242;
+  background-color: #424242;
+}
+
+.spotlight {
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  border-radius: 50%;
+  pointer-events: none;
+  background-image: radial-gradient(circle, rgba(255, 255, 255, 0.5) 50px, transparent 200px);
 }
 </style>
--- src/components/ProjectCarousel.vue
+++ src/components/ProjectCarousel.vue
@@ -10,8 +10,6 @@
   
 <script>
 import * as THREE from 'three';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 export default {
     name: 'ProjectCarousel',
@@ -30,13 +28,14 @@ export default {
             scene.background = new THREE.Color('#191918');
 
             var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+            camera.position.z = 600;
 
             var can = document.getElementById("canvasInfinity");
 
             var renderer = new THREE.WebGLRenderer({ canvas: can, antialiasing: true });
             renderer.setSize(window.innerWidth, window.innerHeight);
 
-            const cubeGeometry = new THREE.BoxGeometry(700, 270, 170);
+            const cubeGeometry = new THREE.BoxGeometry(750, 300, 170);
 
             this.video = document.getElementById('video');
 
@@ -56,7 +55,7 @@ export default {
                     color: '#F0FFFF',
                 }),
                 new THREE.MeshBasicMaterial({
-                    map: videoTexture//texture.load('./test.jpg')
+                    map: videoTexture
                 }),
                 new THREE.MeshStandardMaterial({
                     color: '#F0FFFF',
@@ -65,7 +64,7 @@ export default {
             const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
             scene.add(cube);
 
-            cube.position.set(0, 0, 400);
+            cube.position.set(0, 0, 0);
             cube.rotation.y = Math.PI / 8;
             cube.rotation.x = Math.PI / 8;
 
@@ -84,50 +83,15 @@ export default {
 
             document.addEventListener('mousedown', onDocumentMouseDown, false);
 
-            const fontLoader = new FontLoader();
-            fontLoader.load('/fonts/helvetiker_bold.typeface.json',
-                function (font) {
-                    let textGeometry = new TextGeometry("Projets", {
-                        font: font,
-                        size: 10,
-                        height: 2,
-                        curveSegments: 12,
-                        bevelEnabled: true,
-                        bevelThickness: 1,
-                        bevelSize: 0.5,
-                        bevelOffset: 0,
-                        bevelSegments: 15
-                    });
+            window.addEventListener('resize', function () {
+                camera.aspect = window.innerWidth / window.innerHeight;
+                camera.updateProjectionMatrix();
+                renderer.setSize(window.innerWidth, window.innerHeight)
+            })
 
-                    let textMaterial = new THREE.MeshNormalMaterial();
-                    let text = new THREE.Mesh(textGeometry, textMaterial);
-
-                    if (window.innerWidth < 800) {
-                        text.position.set(-95, 30, 1100);
-                    } else {
-                        text.position.set(-95, 30, 900);
-                    }
-
-                    scene.add(text);
-
-                    if (window.innerWidth < 800) {
-                        camera.position.z = 1500;
-                    } else {
-                        camera.position.z = 1000;
-                    }
-
-                    window.addEventListener('resize', function () {
-                        camera.aspect = window.innerWidth / window.innerHeight;
-                        camera.updateProjectionMatrix();
-                        renderer.setSize(window.innerWidth, window.innerHeight)
-                    })
-
-                    animate();
-                })
+            animate();
 
             function animate() {
-                //cube.rotateOnAxis(axisSphere, 0.005);
-
                 renderer.render(scene, camera);
                 requestAnimationFrame(animate);
             }
